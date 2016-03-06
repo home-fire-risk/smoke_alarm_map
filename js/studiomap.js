9f345f00001a5820fc8473013021b64cfb3c5bf4
Hannah Recht <hrecht@users.noreply.github.com>
2016-03-05 23:04:09 -0500

    yarrghhhh tooltips in filtered view

--- js/studiomap.js
+++ js/studiomap.js
@@ -37,6 +37,38 @@ function makebtns() {
         .classed("selected", true);
 };
 
+function tooltips(layers) {
+    map.on("mousemove", function (e) {
+        map.featuresAt(e.point, {
+            radius: 1,
+            layer: layers
+        }, function (err, features) {
+            if (!err && features.length) {
+                //show name and value in sidebar
+                document.getElementById('tooltip-risk').innerHTML = Math.round(features[0].properties.risk);
+                document.getElementById('tooltip-rank').innerHTML = features[0].properties.rank + "/" + features[0].properties.rankn;
+                document.getElementById('tooltip-households').innerHTML = d3.format(",d")(features[0].properties.hoshlds);
+                document.getElementById('tooltip-income').innerHTML = d3.format("$,d")(features[0].properties.mdnnc_h);
+                if (features[0].properties.NAME != undefined) {
+                    document.getElementById('tooltip-tract').innerHTML = "Tract " + features[0].properties.NAME;
+                }
+                document.getElementById('tooltip-name').innerHTML = features[0].properties.county + "<br />" + features[0].properties.chapter;
+
+                //for troubleshooting - show complete features info
+                //document.getElementById('tooltip-name').innerHTML = JSON.stringify(features, null, 2);
+            } else {
+                //if not hovering over a feature set tooltip to empty
+                document.getElementById('tooltip-risk').innerHTML = "";
+                document.getElementById('tooltip-rank').innerHTML = "";
+                document.getElementById('tooltip-households').innerHTML = "";
+                document.getElementById('tooltip-income').innerHTML = "";
+                document.getElementById('tooltip-tract').innerHTML = "";
+                document.getElementById('tooltip-name').innerHTML = "";
+            }
+        });
+    });
+}
+
 makebtns();
 
 //create map zoomed in to DC
@@ -77,35 +109,7 @@ map.on('style.load', function () {
         });
     }
 
-    map.on("mousemove", function (e) {
-        map.featuresAt(e.point, {
-            radius: 1,
-            layer: RISKLAYERS
-        }, function (err, features) {
-            if (!err && features.length) {
-                //show name and value in sidebar
-                document.getElementById('tooltip-risk').innerHTML = Math.round(features[0].properties.risk);
-                document.getElementById('tooltip-rank').innerHTML = features[0].properties.rank + "/" + features[0].properties.rankn;
-                document.getElementById('tooltip-households').innerHTML = d3.format(",d")(features[0].properties.hoshlds);
-                document.getElementById('tooltip-income').innerHTML = d3.format("$,d")(features[0].properties.mdnnc_h);
-                if (features[0].properties.NAME != undefined) {
-                    document.getElementById('tooltip-tract').innerHTML = "Tract " + features[0].properties.NAME;
-                }
-                document.getElementById('tooltip-name').innerHTML = features[0].properties.county + "<br />" + features[0].properties.chapter;
-
-                //for troubleshooting - show complete features info
-                //document.getElementById('tooltip-name').innerHTML = JSON.stringify(features, null, 2);
-            } else {
-                //if not hovering over a feature set tooltip to empty
-                document.getElementById('tooltip-risk').innerHTML = "";
-                document.getElementById('tooltip-rank').innerHTML = "";
-                document.getElementById('tooltip-households').innerHTML = "";
-                document.getElementById('tooltip-income').innerHTML = "";
-                document.getElementById('tooltip-tract').innerHTML = "";
-                document.getElementById('tooltip-name').innerHTML = "";
-            }
-        });
-    });
+    tooltips(['east1', 'east2', 'east3', 'east4', 'east5', 'east6', 'south1', 'south2', 'south3', 'south4', 'south5', 'south6', 'midwest1', 'midwest2', 'midwest3', 'midwest4', 'midwest5', 'midwest6', 'west1', 'west2', 'west3', 'west4', 'west5', 'west6', 'counties1', 'counties2', 'counties3', 'counties4', 'counties5', 'counties6', 'easttop', 'southtop', 'midwesttop', 'westtop']);
 
 });
 
@@ -153,37 +157,8 @@ $('#statbtns input:radio').click(function (e) {
         for (r = 0; r < REGIONS.length; r++) {
             var filtertype = map.getFilter(REGIONS[r] + 'top');
             map.setFilter(REGIONS[r] + 'top', ['<=', "rank", n]);
-            
-        }
-        map.on("mousemove", function (e) {
-            map.featuresAt(e.point, {
-                radius: 10,
-                layer: ['easttop', 'southtop', 'midwesttop', 'westtop']
-            }, function (err, features) {
-                if (!err && features.length) {
-                    //show name and value in sidebar
-                    document.getElementById('tooltip-risk').innerHTML = Math.round(features[0].properties.risk);
-                    document.getElementById('tooltip-rank').innerHTML = features[0].properties.rank + "/" + features[0].properties.rankn;
-                    document.getElementById('tooltip-households').innerHTML = d3.format(",d")(features[0].properties.hoshlds);
-                    document.getElementById('tooltip-income').innerHTML = d3.format("$,d")(features[0].properties.mdnnc_h);
-                    if (features[0].properties.NAME != undefined) {
-                        document.getElementById('tooltip-tract').innerHTML = "Tract " + features[0].properties.NAME;
-                    }
-                    document.getElementById('tooltip-name').innerHTML = features[0].properties.county + "<br />" + features[0].properties.chapter;
 
-                    //for troubleshooting - show complete features info
-                    //document.getElementById('tooltip-name').innerHTML = JSON.stringify(features, null, 2);
-                } else {
-                    //if not hovering over a feature set tooltip to empty
-                    document.getElementById('tooltip-risk').innerHTML = "boo";
-                    document.getElementById('tooltip-rank').innerHTML = "";
-                    document.getElementById('tooltip-households').innerHTML = "";
-                    document.getElementById('tooltip-income').innerHTML = "";
-                    document.getElementById('tooltip-tract').innerHTML = "";
-                    document.getElementById('tooltip-name').innerHTML = "";
-                }
-            });
-        });
+        }        
     }
 });
 
