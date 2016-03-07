be530f40c2ebe95fbc0b60c4718c254d8a8ce4ca
d3netxer <tommygun320@yahoo.com>
2016-03-06 20:34:33 -0500

    adding a legend

---
+++ js/legend.js
@@ -0,0 +1,22 @@
+var colors = d3.scale.quantize()
+.range(colorbrewer.OrRd[6]);
+
+var legend = d3.select('#legend')
+  .append('ul')
+    .attr('class', 'list-inline')
+    .attr('style' , 'list-style-type:none');
+
+var keys = legend.selectAll('li.key')
+    .data(colors.range());
+
+keys.enter().append('li')
+    .attr('class', 'key')
+    .style('border-left-color', String)
+    .text(function(d) {
+        var r = colors.invertExtent(d);
+        //return formats.percent(r[0]);
+    });
+
+legend.selectAll('li.key').html('&nbsp;');
+legend.select('li.key').html('&nbsp;&nbsp;&nbsp;&nbsp;Least&nbsp;Risk');
+legend.select('li.key:last-of-type').html('&nbsp;&nbsp;&nbsp;&nbsp;Most&nbsp;Risk');
